7ef4a8452bde432fc5e50194fc7e512e9ef9c51e
AnJuHwan <wnghks0531@gmail.com>
2021-11-24 07:17:23 +0900

    setting

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import BackwardCounter from './components/BackwardCounter.jsx';
+import ForwardCounter from './components/ForwardCounter.jsx';
+
+function App() {
+  return (
+    <>
+      <ForwardCounter />
+      <BackwardCounter />
+    </>
+  );
+}
+
+export default App;
